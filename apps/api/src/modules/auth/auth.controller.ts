73e20b3dd3777b0f470d64e7ad9f3c210974df64
fazlulShanto <fazlulcse17@gmail.com>
2025-05-25 00:28:30 +0600

    fix: cookie error in https:

--- apps/api/src/modules/auth/auth.controller.ts
+++ apps/api/src/modules/auth/auth.controller.ts
@@ -61,10 +61,11 @@ export default class AuthController {
     reply.setCookie("token", jwtToken, {
       path: "/",
       httpOnly: true,
-      secure: false, // Set to false for HTTP in development
+      secure: ["prod", "stage"].includes(process.env.NODE_ENV),
       sameSite: "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
     });
+
     return reply.redirect(
       `${atob(clientRedirectUrl)}/onboarding?auth_token=${jwtToken}`
     );
