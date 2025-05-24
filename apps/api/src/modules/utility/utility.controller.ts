ca8b3a007547535182cc29b4fe2d27c6718f6542
fazlulShanto <fazlulcse17@gmail.com>
2025-05-25 01:06:13 +0600

    fix: cookie error in https:v2

--- apps/api/src/modules/utility/utility.controller.ts
+++ apps/api/src/modules/utility/utility.controller.ts
@@ -2,48 +2,64 @@ import type { FastifyReply, FastifyRequest } from "fastify";
 import type UtilityService from "./utility.service";
 
 export default class UtilityController {
-    private utilityService: UtilityService;
+  private utilityService: UtilityService;
 
-    constructor(utilityService: UtilityService) {
-        this.utilityService = utilityService;
+  constructor(utilityService: UtilityService) {
+    this.utilityService = utilityService;
+  }
+
+  public async getApiUptimeHandler(
+    request: FastifyRequest,
+    reply: FastifyReply
+  ) {
+    try {
+      const uptime = this.utilityService.getApiUptime();
+
+      return reply.code(200).send(uptime);
+    } catch (e) {
+      if (e instanceof Error) {
+        return reply.badRequest(e.message);
+      }
+
+      /* istanbul ignore next */
+      throw e;
     }
+  }
 
-    public async getApiUptimeHandler(
-        request: FastifyRequest,
-        reply: FastifyReply
-    ) {
-        try {
-            const uptime = this.utilityService.getApiUptime();
-
-            return reply.code(200).send(uptime);
-        } catch (e) {
-            if (e instanceof Error) {
-                return reply.badRequest(e.message);
-            }
-
-            /* istanbul ignore next */
-            throw e;
-        }
+  public async getApiStatusHandler(
+    request: FastifyRequest,
+    reply: FastifyReply
+  ) {
+    try {
+      const status = this.utilityService.getApiStatus();
+      return reply.code(200).send(status);
+    } catch (e) {
+      return reply.badRequest("Something went wrong");
     }
+  }
 
-    public async getApiStatusHandler(
-        request: FastifyRequest,
-        reply: FastifyReply
-    ) {
-        try {
-            const status = this.utilityService.getApiStatus();
-            return reply.code(200).send(status);
-        } catch (e) {
-            return reply.badRequest("Something went wrong");
-        }
+  public async getServerHardwareInfoHandler(
+    request: FastifyRequest,
+    reply: FastifyReply
+  ) {
+    const info = this.utilityService.getServerHardwareInfo();
+
+    return reply.code(200).send(info);
+  }
+
+  public async cookieTestHandler(request: FastifyRequest, reply: FastifyReply) {
+    const isSecure = request.protocol === "https";
+    const cookiesData = request.body as Record<string, string>;
+    const keys = Object.keys(cookiesData);
+    for (const key of keys) {
+      reply.setCookie(key, cookiesData[key], {
+        path: "/",
+        httpOnly: true,
+        secure: isSecure,
+        sameSite: "lax",
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
+      });
     }
-
-    public async getServerHardwareInfoHandler(
-        request: FastifyRequest,
-        reply: FastifyReply
-    ) {
-        const info = this.utilityService.getServerHardwareInfo();
-
-        return reply.code(200).send(info);
-    }
+    return reply.code(200).send({ message: "done", cookiesData });
+  }
 }
--- apps/api/src/modules/utility/utility.route.ts
+++ apps/api/src/modules/utility/utility.route.ts
@@ -4,61 +4,75 @@ import { $ref } from "./utility.schema";
 import UtilityService from "./utility.service";
 
 export default function UtilityRoute(fastify: FastifyInstance) {
-    const utilityController = new UtilityController(new UtilityService());
+  const utilityController = new UtilityController(new UtilityService());
 
-    fastify.get(
-        "/uptime",
-        {
-            schema: {
-                tags: ["Utility"],
-                description: "Get the uptime of the API",
-                response: {
-                    200: $ref("getApiUptimeResponseSchema"),
-                },
-            },
+  fastify.get(
+    "/uptime",
+    {
+      schema: {
+        tags: ["Utility"],
+        description: "Get the uptime of the API",
+        response: {
+          200: $ref("getApiUptimeResponseSchema"),
         },
-        utilityController.getApiUptimeHandler.bind(utilityController)
-    );
+      },
+    },
+    utilityController.getApiUptimeHandler.bind(utilityController)
+  );
 
-    fastify.get(
-        "/health-check",
-        {
-            schema: {
-                tags: ["Utility"],
-                description: "Get the status of the API if it's running",
-                response: {
-                    200: $ref("getApiStatusResponseSchema"),
-                },
-            },
+  fastify.get(
+    "/health-check",
+    {
+      schema: {
+        tags: ["Utility"],
+        description: "Get the status of the API if it's running",
+        response: {
+          200: $ref("getApiStatusResponseSchema"),
         },
-        utilityController.getApiStatusHandler.bind(utilityController)
-    );
+      },
+    },
+    utilityController.getApiStatusHandler.bind(utilityController)
+  );
 
-    fastify.get(
-        "/ping",
-        {
-            schema: {
-                tags: ["Utility"],
-                description: "Ping the API to check if it's running",
-                response: {
-                    200: $ref("getApiStatusResponseSchema"),
-                },
-            },
+  fastify.get(
+    "/ping",
+    {
+      schema: {
+        tags: ["Utility"],
+        description: "Ping the API to check if it's running",
+        response: {
+          200: $ref("getApiStatusResponseSchema"),
         },
-        utilityController.getApiStatusHandler.bind(utilityController)
-    );
+      },
+    },
+    utilityController.getApiStatusHandler.bind(utilityController)
+  );
 
-    fastify.get(
-        "/server-info",
-        {
-            schema: {
-                tags: ["Utility"],
-                description: "Get the server hardware information",
-                response: {
-                    200: $ref("getServerHardwareInfoResponseSchema"),
-                },
-            },
+  fastify.get(
+    "/server-info",
+    {
+      schema: {
+        tags: ["Utility"],
+        description: "Get the server hardware information",
+        response: {
+          200: $ref("getServerHardwareInfoResponseSchema"),
         },
-        utilityController.getServerHardwareInfoHandler.bind(utilityController)
-    );
+      },
+    },
+    utilityController.getServerHardwareInfoHandler.bind(utilityController)
+  );
+
+  fastify.post(
+    "/cookie-test",
+    {
+      schema: {
+        tags: ["Utility"],
+        description: "Test the cookie",
+        body: {
+          type: "object",
+        },
+      },
+    },
+    utilityController.cookieTestHandler.bind(utilityController)
+  );
 }
